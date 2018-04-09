276b267b75b36ee5300f102ed76dd217144fd138
jdperezv <jdperezv@unal.edu.co>
2018-04-08 23:22:20 -0500

    Add enemies list

--- praxis/src/components/HeroeOverview/HeroeOverview.js
+++ praxis/src/components/HeroeOverview/HeroeOverview.js
@@ -5,9 +5,8 @@ class HeroeOverview extends Component {
     render() {
         return (<div className='heroe-overview-container'>
             <img src={this.props.image} alt={this.props.name} />
-            <h1>{this.props.name}</h1>
-            <p className='heroe-overview-text'>{this.props.overview}</p>
-            <a href={this.props.link}>More info..</a>
+            <a href={this.props.link}><h1>{this.props.name}</h1></a>
+            <p className='heroe-overview-text'>{this.props.overview} </p>
         </div>);
     }
 }
---
+++ praxis/src/services/enemiesService.js
@@ -0,0 +1,8 @@
+
+import axios from 'axios';
+
+export default {
+    getEnemies: function () {
+        return axios.get('https://jdtorregrosasapi.herokuapp.com/enemies');
+    }
+};
